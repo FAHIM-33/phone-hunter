6079a3b534619a46d6d06a34681173f7e85a82b3
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-08-28 01:03:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let loadPhone = async () => {
+    let res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+   let arr = await res.json();
+   let phones = arr.data;
+   handler(phones)
+}
+function handler(phones){
+    const parent = document.getElementById('cards-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        let div = document.createElement('div');
+        div.classList = `card flex flex-col items-center p-8 rounded-lg shadow-2xl`;
+        div.innerHTML = `
+        <figure>
+        <img src=${phone.image} alt="">
+    </figure>
+    <h3 class="t-header my-5 text-center">${phone.phone_name}</h3>
+    <p class="text-center mb-2 plain-text">There are many variations of passages of available, but the majority have suffered</p>
+    <p class="t-header">$999</p>
+    <button class="main-btn mt-4">Show Details</button>
+        `
+        parent.appendChild(div);
+    })
+}
+
+loadPhone()
